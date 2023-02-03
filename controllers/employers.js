9fafa133de33653a209c618135d35e1928077a4c
Tyler Miele <tylermiele10@gmail.com>
2023-02-03 17:39:57 -0500

    Finished project setup

--- controllers/employers.js
+++ controllers/employers.js
@@ -1,11 +1,43 @@
 const express = require('express');
 const router = express.Router();
 
+//add fs to read the data file
+const fs = require('fs');
+
+
 /* GET employers index (the module home page) */
-router.get('/', (req, res) =>{
+router.get('/', (req, res) => {
+    const employers = [
+        {
+            "name": "Provix"
+        },
+        {
+            "name": "Element6"
+        },
+        {
+            "name": "Netgain"
+        },
+        {
+            "name": "44 North"
+        }
+    ];
     res.render('employers/index', {
-        title: "Employers List"
+        title: "Employers List",
+        employers: employers
     });
+
+
+    // get data from JSON file
+    // fs.readFile('./data/employers.json', 'utf8', (err, employers) =>{
+    //     if (err){
+    //         console.log(err);
+    //     }else{
+    //         res.render('employers/index', {
+    //             title: "Employers List",
+    //             employers: employers
+    //         });
+    //     }
+    // });
 });
 
 //export to make this file public
